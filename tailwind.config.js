96000d46c498898201e6078c91f6ec85689d9e51
mrrpablo <precijagz480@gmail.com>
2023-08-01 18:10:59 +0100

    my-portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'back': "url('/images/back.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
